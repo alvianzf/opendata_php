7903e1ba9cdd3d5a3d31ef90a6290136168d989c
alvianzf <alvianzf@gmail.com>
2024-09-30 20:53:04 +0700

    refactor: config

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 
-$q = mysql_connect("localhost", "root", "");
-mysql_select_db("opendatadb");
+$databaseConnection = mysqli_connect("localhost", "root", "");
+mysqli_select_db($databaseConnection, "opendatadb");
 
-// if ($q)
+// if ($databaseConnection)
 // {
-//     echo "tersambung";
+//     echo "Connected successfully";
 // }
 
-// // $sql = mysql_query("INSERT INTO tb_dataset VALUES (NULL, 'TEST 2', 'TEST DATASET 2', ("2)");
-// mysql_query("INSERT INTO tb_user VALUES (NULL, '1234567890', 'admin', 1)");
-// mysql_query("INSERT INTO tb_peg VALUES ('1234567890', 'Rio Putra', 'SETDAKO', 1)");
+// // $insertDatasetQuery = mysqli_query($databaseConnection, "INSERT INTO tb_dataset VALUES (NULL, 'TEST 2', 'TEST DATASET 2', '2')");
+// mysqli_query($databaseConnection, "INSERT INTO tb_user VALUES (NULL, '1234567890', 'admin', 1)");
+// mysqli_query($databaseConnection, "INSERT INTO tb_peg VALUES ('1234567890', 'Rio Putra', 'SETDAKO', 1)");
