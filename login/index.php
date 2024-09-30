64d0cec9ab54baa2564b384fcda8101ed7663782
alvianzf <alvianzf@gmail.com>
2024-09-30 20:46:30 +0700

    refactor: login

--- login/index.php
+++ login/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Login Page</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/Google-Style-Login.css">
@@ -12,19 +12,23 @@
 </head>
 
 <body>
-    <div class="login-card"><img src="assets/img/logo.jpeg" class="profile-img-card">
-        <p class="profile-name-card"> </p>
-        <form class="form-signin" method="get" action="login.php"><span class="reauth-email"> <span>OpenData Tanjungpinang</span></span>
-            <input class="form-control" type="text" required="" placeholder="NIP" autofocus="" name="uname">
-            <input class="form-control" type="password" required="" placeholder="Password" name="pass">
-            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Login</button>
+    <div class="login-container">
+        <img src="assets/img/logo.jpeg" class="profile-image" alt="Profile Image">
+        <p class="profile-name"></p>
+        <form class="login-form" method="get" action="login.php">
+            <span class="reauth-email"><span>OpenData Tanjungpinang</span></span>
+            <input class="form-control" type="text" required placeholder="NIP" autofocus name="username">
+            <input class="form-control" type="password" required placeholder="Password" name="password">
+            <button class="btn btn-primary btn-block btn-lg login-button" type="submit">Login</button>
         </form>
     </div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="controls/DataTables/datatables.min.js"></script>
     <script>
-        $('.table').DataTable();
+        $(document).ready(function() {
+            $('.table').DataTable();
+        });
     </script>
 </body>
 
--- login/login.php
+++ login/login.php
@@ -3,18 +3,19 @@
 include '../config/config.php';
 include '../functions/sql.php';
 
-$u = @$_GET['uname'];
-$p = @$_GET['pass'];
+$username = isset($_GET['uname']) ? $_GET['uname'] : '';
+$password = isset($_GET['pass']) ? $_GET['pass'] : '';
 
-$resp = login($u, $p);
+$loginResult = login($username, $password);
 
-echo $resp;
-if ($resp == 1)
+echo $loginResult;
+if ($loginResult == 1)
 {
-    header('location:../admin/status.php?r=1&u='.$u);
-
-}else{
-    header('location:../login/index.php?r=failed');
+    header('Location: ../admin/status.php?r=1&u=' . urlencode($username));
+}
+else
+{
+    header('Location: ../login/index.php?r=failed');
 }
 
 ?>
